c88a39c568c1e782984b43f76404f1b0d46e0f54
Arslaan123Iqbal <arslaniqbalmgt@gmail.con>
2024-03-07 11:28:50 +0500

    testing images locally added to s3 succesfully

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "m33r": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,12 +2,32 @@ import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/api';
 import * as mutations from './graphql/mutations';
 import { listTodos } from './graphql/queries';
+import { uploadData } from 'aws-amplify/storage';
 const client = generateClient();
 
 function TodoList() {
   const [todo, setTodo] = useState('');
   const [todos, setTodos] = useState([]);
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false)
 
+  const uploadFile = async () => {
+    try {
+      const result = await uploadData({
+        key: fileData.name,
+        data: fileData,
+        options: {
+          accessLevel: 'guest', // Adjust the access level as needed
+          // You can include an optional progress callback if needed
+          // onProgress: (progress) => console.log(`Progress: ${progress.loaded}/${progress.total}`)
+        }
+      });
+      console.log('Succeeded: ', result); // Log the successful upload result
+      setFileStatus(true);
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
   useEffect(() => {
     const fetchTodos = async () => {
       try {
@@ -79,6 +99,11 @@ const addTodo = async (newTodo) => {
      <div className='text-center'>
      <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} className='border-1 rounded bg-red-100 p-1 ' />
       <button onClick={() => addTodo(todo)} className='py-1 px-2 bg-[green] rounded inline-block m-2 text-white'>Add todo</button>
+     </div>
+     <div>
+     <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+     <button onClick={uploadFile} className='py-1 px-2 bg-[green] rounded inline-block m-2 text-white'>Upload File</button>
+      {fileStatus ? 'file uploaded successfule':''}
      </div>
       {todos.map((todoItem) => (
         <div key={todoItem.id} className='p-2 border rounded my-2'>
